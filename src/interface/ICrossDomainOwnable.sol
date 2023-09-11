b88ad4cc7802774ea40bcaa04509ae597bf137c1
Haythem Sellami <sellamihaythem96@gmail.com>
2023-09-11 18:03:35 +0700

    feat: add interface

---
+++ src/interface/ICrossDomainOwnable.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface CrossDomainOwnable {
+    /// @notice Allows for ownership to be transferred with specifying the locality.
+    /// @param _owner   The new owner of the contract.
+    /// @param _isLocal Configures the locality of the ownership.
+    function transferOwnership(address _owner, bool _isLocal) external;
+}
